1c63814f1ec6210a9dd9b0d8244b873d780d3a66
Max Evans <marvmax1@msn.com>
2022-01-25 05:47:21 -0700

    Initial Commit

---
+++ RandomScriptures/RandomScripturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomScripturesApp.swift
+//  RandomScriptures
+//
+//  Created by Max Evans on 1/25/22.
+//
+
+import SwiftUI
+
+@main
+struct RandomScripturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
